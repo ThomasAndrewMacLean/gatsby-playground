6f62157a26ec0a13ade0f0309f9bfab5092bf804
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-02-10 19:45:55 +0100

    test

--- src/pages/calender.js
+++ src/pages/calender.js
@@ -2,7 +2,6 @@ import React from 'react';
 // import { Link } from 'gatsby'
 
 import Layout from '../components/layout';
-import Helmet from 'react-helmet';
 
 const IndexPage = () => (
   <Layout>
